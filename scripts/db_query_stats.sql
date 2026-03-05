df5f7de0a81cf03cf1d20357c4510d7748d01610
Merlijn Tishauser <merlijn@ccadd.nl>
2026-03-05 21:47:13 +0100

    Fix pg_stat_statements query to use dbid instead of dbname

--- scripts/db_query_stats.sql
+++ scripts/db_query_stats.sql
@@ -6,7 +6,7 @@ SELECT
     ROUND((max_exec_time)::numeric, 2) AS max_ms,
     rows
 FROM pg_stat_statements
-WHERE dbname = 'traumabomen'
+WHERE dbid = (SELECT oid FROM pg_database WHERE datname = 'traumabomen')
   AND query NOT LIKE '%pg_stat%'
 ORDER BY total_exec_time DESC
 LIMIT 20;
